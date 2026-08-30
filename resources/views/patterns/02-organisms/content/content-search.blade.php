6d38c04bec4b35b60d9c7c2feced463065d6e738
DariusE <darius.eic@gmail.com>
2026-08-30 15:34:30 +0300

    Translate fallback search interface to Lithuanian

--- resources/views/patterns/02-organisms/content/content-search.blade.php
+++ resources/views/patterns/02-organisms/content/content-search.blade.php
@@ -2,11 +2,11 @@
   <form action="<?php echo esc_url(home_url('/')); ?>" role="search" method="get">
     <div class="l-grid l-grid--7-col u-shift--left--1-col--at-large u-spacing--until-medium">
       <div class="l-grid-item l-grid-item--m--3-col">
-        <input type="search" value="{{ the_search_query() }}" name="s" class="u-font--secondary--s u-theme--color--darker o-input__search" placeholder="{{ __('Search...', 'alps') }}" />
+        <input type="search" value="{{ the_search_query() }}" name="s" class="u-font--secondary--s u-theme--color--darker o-input__search" placeholder="Ieškoti..." />
       </div>
       <div class="l-grid-item l-grid-item--m--3-col">
         <div class="u-flex">
-          <button type="submit" class="c-filter__button o-button u-space--right"><span class="u-icon u-icon--xs u-path-fill--white">@include('patterns.00-atoms.icons.icon-search')</span>{{ __('Search', 'alps') }}</button> <!-- /.search-form__submit -->
+          <button type="submit" class="c-filter__button o-button u-space--right"><span class="u-icon u-icon--xs u-path-fill--white">@include('patterns.00-atoms.icons.icon-search')</span>Ieškoti</button> <!-- /.search-form__submit -->
           @if (shortcode_exists('searchandfilter'))
             <span class="c-filter__toggle js-toggle o-button o-button--simple" data-toggled="c-filter" data-prefix="c-filter"><span class="u-icon u-icon--xs">@include('patterns.00-atoms.icons.icon-settings')</span></span>
           @endif
@@ -19,7 +19,7 @@
       <div class="c-filter__form u-padding--top">
         <div class="l-grid l-grid--7-col u-shift--left--1-col--at-large">
           <div class="c-filter__form-item u-spacing--half l-grid-item">
-            <h3 class="u-font--secondary--s u-font-weight--bold u-text-transform--upper u-color--gray can-be--lighter">{{ __('Settings', 'alps') }}</h3>
+            <h3 class="u-font--secondary--s u-font-weight--bold u-text-transform--upper u-color--gray can-be--lighter">Nustatymai</h3>
             {!! do_shortcode('[searchandfilter taxonomies="category" types="radio" class="c-filter__form-group" submit_label="Ieškoti dar kartą" hide_empty="0"]') !!}
           </div>
         </div>
@@ -41,7 +41,7 @@
               $excerpt_length = 55;
               $body = get_the_content($id);
               $link = get_permalink($id);
-              $cta = __("Read More", "alps");
+              $cta = "Skaityti daugiau";
               $block_class = "u-theme--border-color--darker u-border--left u-spacing--half";
             @endphp
             @include('patterns.01-molecules.blocks.content-block')
@@ -52,7 +52,7 @@
             @php pagination_nav() @endphp
           @endif
         @else
-          <p>{{ __('Sorry, no results were found.', 'alps') }}</p>
+          <p>Atsiprašome, rezultatų nerasta.</p>
           {!! get_search_form(false) !!}
         @endif
       </div>
