995a90f3714956f1f59f88f006be3a2e74736387
DariusE <darius.eic@gmail.com>
2026-08-30 15:53:02 +0300

    Translate generic search form to Lithuanian

--- resources/views/forms/search.blade.php
+++ resources/views/forms/search.blade.php
@@ -1,16 +1,14 @@
 <form role="search" method="get" class="search-form" action="{{ home_url('/') }}">
   <label>
-    <span class="sr-only">
-      {{ _x('Search for:', 'label', 'alps') }}
-    </span>
+    <span class="sr-only">Ieškoti:</span>
 
     <input
       type="search"
-      placeholder="{!! esc_attr_x('Search &hellip;', 'placeholder', 'alps') !!}"
+      placeholder="Ieškoti…"
       value="{{ get_search_query() }}"
       name="s"
     >
   </label>
 
-  <button>{{ _x('Search', 'submit button', 'alps') }}</button>
+  <button>Ieškoti</button>
 </form>
