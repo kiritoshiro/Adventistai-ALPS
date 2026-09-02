100ca907fe2051a9154e82f4de52b299e8c40307
DariusE <darius.eic@gmail.com>
2026-09-02 16:43:59 +0300

    Restore ALPS editor fields and hide empty header image
    Upgrade Carbon Fields to 3.6.11 for current WordPress block-editor compatibility. Render the simple title header whenever no usable featured image exists, and release theme version 3.22.9.

--- resources/views/patterns/01-molecules/blocks/header-block-featured.blade.php
+++ resources/views/patterns/01-molecules/blocks/header-block-featured.blade.php
@@ -11,17 +11,26 @@
    * @var string $headerCategory
    * @var string $headerImages
    */
-    $mediaBlockTitle = $headerTitle;
-    $mediaBlockTitleLink = '';
-    $mediaBlockDesc = $headerDesc;
-    $mediaBlockDate = $headerDate;
-    $mediaBlockCategory = $headerCategory;
-    $mediaBlockImageCaption = $headerImageCaption;
-    $mediaBlockImages = $headerImages;
-    $mediaBlockKicker = $headerKicker;
+    $hasHeaderImage = !empty($headerImages['s'][0]);
+
+    if ($hasHeaderImage) {
+      $mediaBlockTitle = $headerTitle;
+      $mediaBlockTitleLink = '';
+      $mediaBlockDesc = $headerDesc;
+      $mediaBlockDate = $headerDate;
+      $mediaBlockCategory = $headerCategory;
+      $mediaBlockImageCaption = $headerImageCaption;
+      $mediaBlockImages = $headerImages;
+      $mediaBlockKicker = $headerKicker;
+    }
 @endphp
-<header class="c-page-header c-page-header__feature">
-  <div class="c-page-header__content">
-    @include('patterns.01-molecules.blocks.media-block-v2')
-  </div>
-</header>
+
+@if ($hasHeaderImage)
+  <header class="c-page-header c-page-header__feature">
+    <div class="c-page-header__content">
+      @include('patterns.01-molecules.blocks.media-block-v2')
+    </div>
+  </header>
+@else
+  @include('patterns.01-molecules.blocks.header-block')
+@endif
